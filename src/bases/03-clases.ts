99e620339aa1876fc6fcad50f22e3155fda75b09
williner.martin <williner.martin@gmail.com>
2024-06-05 15:13:24 +0000

    getters, metodos y this

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,10 +1,24 @@
 // clases son representaciones de objetos en el mundo real
 export class Pokemon {
+    //getters - sirven para obtener un valor de una propiedad privada
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`
+    }
 
-    constructor(public readonly id: number, public name: string) {}
+    constructor(
+        public readonly id: number,
+        public name: string,
+        ) {}
     // readOnly es una propiedad que no se puede modificar, solo se puede leer
+
+        // metodos - son acciones que hace un objeto
+    public scream() {
+        console.log(`GO ${this.name}!`)
+    }
+    public speak() {
+        console.log(`Hello, I'm ${this.name}`)
+    }
+
 }
 
 export const pikachu = new Pokemon(1, 'pikachu');
-export const charmander = new Pokemon(2, 'charmander');
-export const bulbasaur = new Pokemon(3, 'bulbasaur');
