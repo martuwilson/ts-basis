4fb92d9117c089a36ae21c1e5d3ea1e7d3c7ffe1
williner.martin <williner.martin@gmail.com>
2024-06-05 14:13:43 +0000

    ts types

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import { name, age } from './bases/01-types'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h2>{name}</h2>
+      <p>{age}</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/bases/01-types.ts
@@ -0,0 +1,14 @@
+
+export const name: string = 'Martutester';
+
+export const age: number = 10;
+
+export const isValid: boolean = true;
+
+
+export const templatestring = `Esto es un string multilinea
+con salto de linea
+y tabulacion
+inyectar valores ${name}
+expresiones ${2 + 2}
+boolenos ${isValid}`
